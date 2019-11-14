b0d818c1c014de82be30fa184fa26e3c906ea5c1
karen <kjr1@queensu.ca>
2019-11-14 13:42:09 -0500

    Adds a dictionary of correlated signatures

---
+++ mutagene/signatures/constants.py
@@ -0,0 +1,54 @@
+correlated_sig = [
+    {
+        'description': 'UV radiation',
+        'sig': {5: ["4"], 10: ["9"], 30: ["7"], 49: ["SBS7"]}
+    },
+    {
+        'description': 'Tobacco',
+        'sig': {5: ["1"], 10: ["4"], 30: ["4"], 49: ["SBS4"]}
+    },
+    {
+        'description': 'Aflatoxin',
+        'sig': {5: ["1"], 10: ["4"], 30: ["24"], 49: ["SBS24"]}
+    },
+    {
+        'description': 'AID/APOBEC',
+        'sig': {5: ["2"], 10: ["5"], 30: ["2", "13"], 49: ["SBS2", "SBS13"]}
+    },
+    {
+        'description': 'Deamination of methyl-cytosine',
+        'sig': {5: ["5"], 10: ["1"], 30: ["1"], 49: ["SBS1"]}
+    },
+    {
+        'description': 'Aristolochic acid',
+        'sig': {5: [], 10: ["3"], 30: ["22"], 49: ["SBS22"]}
+    },
+    {
+        'description': 'Defective DNA mismatch repair',
+        'sig': {5: [], 10: ["10"], 30: ["20", "26"], 49: ["SBS6", "SBS26"]}
+    },
+    {
+        'description': 'DNA polymerase epsilon',
+        'sig': {5: [], 10: ["8"], 30: [], 49: ["SBS10"]}
+    },
+    {
+        'description': 'DNA polymerase eta',
+        'sig': {5: [], 10: [], 30: ["9"], 49: ["SBS9"]}
+    },
+    {
+        'description': 'Failure DNA recombination',
+        'sig': {5: [], 10: [], 30: ["3"], 49: ["SBS3"]}
+    },
+    {
+        'description': 'Alkylating agents',
+        'sig': {5: [], 10: [], 30: ["11"], 49: ["SBS11"]}
+    },
+    {
+        'description': 'DNA mismatch repair',
+        'sig': {5: [], 10: [], 30: ["15"], 9: ["SBS6","SBS15","SBS21","SBS44"]}
+    },
+    {
+        'description': 'Tobacco chewing',
+        'sig': {5: [], 10: [], 30: ["29"], 49: ["SBS29"]}
+    }
+]
