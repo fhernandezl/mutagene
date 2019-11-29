6e9c42d13ac922f90e4eea2a3394ab120675ab1a
karen <kjr1@queensu.ca>
2019-11-29 10:01:11 -0500

    Karen updated etiology dictionary

--- mutagene/signatures/constants.py
+++ mutagene/signatures/constants.py
@@ -1,58 +1,58 @@
-correlated_sig = [
+sig_by_etiology = [
     {
-        'description': 'UV radiation',
+        'etiology': 'UV radiation',
         'sig': {5: ["4"], 10: ["9"], 30: ["7"], 49: ["SBS7a", "SBS7b", "SBS7c", "SBS7d"]}
     },
     {
-        'description': 'Tobacco',
+        'etiology': 'Tobacco',
         'sig': {5: [], 10: [], 30: ["4"], 49: ["SBS4"]}
     },
     {
-        'description': 'Aflatoxin',
+        'etiology': 'Aflatoxin',
         'sig': {5: [], 10: [], 30: ["24"], 49: ["SBS24"]}
     },
     {
-        'description': 'Tobacco and Aflatoxin',
-        'sig': {5: ["1"], 10: ["4"], 30: [], 49: []}
+        'etiology': 'Tobacco and Aflatoxin',
+        'sig': {5: ["1"], 10: ["4"], 30: ["4", "24"], 49: ["SBS4", "SBS24"]}
     },
     {
-        'description': 'AID/APOBEC',
+        'etiology': 'AID/APOBEC',
         'sig': {5: ["2"], 10: ["5"], 30: ["2", "13"], 49: ["SBS2", "SBS13"]}
     },
     {
-        'description': 'Deamination of methyl-cytosine',
+        'etiology': 'Deamination of methyl-cytosine',
         'sig': {5: ["5"], 10: ["1"], 30: ["1"], 49: ["SBS1"]}
     },
     {
-        'description': 'Aristolochic acid',
+        'etiology': 'Aristolochic acid',
         'sig': {5: [], 10: ["3"], 30: ["22"], 49: ["SBS22"]}
     },
     {
-        'description': 'Defective DNA mismatch repair',
+        'etiology': 'Defective DNA mismatch repair',
         'sig': {5: [], 10: ["10"], 30: ["20", "26"], 49: ["SBS6", "SBS26"]}
     },
     {
-        'description': 'DNA polymerase epsilon',
+        'etiology': 'DNA polymerase epsilon',
         'sig': {5: [], 10: ["8"], 30: [], 49: ["SBS10a", "SBS10b"]}
     },
     {
-        'description': 'DNA polymerase eta',
+        'etiology': 'DNA polymerase eta',
         'sig': {5: [], 10: [], 30: ["9"], 49: ["SBS9"]}
     },
     {
-        'description': 'Failure DNA recombination',
+        'etiology': 'Failure DNA recombination',
         'sig': {5: [], 10: [], 30: ["3"], 49: ["SBS3"]}
     },
     {
-        'description': 'Alkylating agents',
+        'etiology': 'Alkylating agents',
         'sig': {5: [], 10: [], 30: ["11"], 49: ["SBS11"]}
     },
     {
-        'description': 'DNA mismatch repair',
+        'etiology': 'DNA mismatch repair',
         'sig': {5: [], 10: [], 30: ["15"], 49: ["SBS6","SBS15","SBS21","SBS44"]}
     },
     {
-        'description': 'Tobacco chewing',
+        'etiology': 'Tobacco chewing',
         'sig': {5: [], 10: [], 30: ["29"], 49: ["SBS29"]}
     }
 ]
